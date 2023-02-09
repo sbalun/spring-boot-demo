9dd860e2ef374601a55ca92c0da22c803dae6312
Scott Balun <sbalun@gmail.com>
2023-02-09 18:07:51 -0500

    file clean up

---
+++ src/main/java/com/balun/sprintbootdemo/SprintBootDemoApplication.java
@@ -0,0 +1,14 @@
+package com.balun.sprintbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintBootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintBootDemoApplication.class, args);
+	}
+
+
+}
---
+++ src/test/java/com/balun/sprintbootdemo/SprintBootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.balun.sprintbootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintBootDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
