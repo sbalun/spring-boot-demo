a1f3159ea769ccf23378315bff4f8e4c4a49be50
Scott Balun <sbalun@gmail.com>
2023-02-09 19:37:04 -0500

    Removed initial code from main method, added the StudentController class with the code removed from main

--- src/main/java/com/balun/sprintbootdemo/SprintBootDemoApplication.java
+++ src/main/java/com/balun/sprintbootdemo/SprintBootDemoApplication.java
@@ -1,22 +1,19 @@
 package com.balun.sprintbootdemo;
 
+import com.balun.sprintbootdemo.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.List;
 
 @SpringBootApplication
-@RestController
 public class SprintBootDemoApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SprintBootDemoApplication.class, args);
-	}
-	@GetMapping("/")
-	public List<String> hello(){
-		return List.of("Hello", "World");
 	}
 
 }
