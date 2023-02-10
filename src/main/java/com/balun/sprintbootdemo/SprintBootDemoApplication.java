f85660577c6e792b904a47aa9daed832684a7a01
Scott Balun <sbalun@gmail.com>
2023-02-09 19:07:12 -0500

    add student class, update gitignore to remove app.prop file

--- src/main/java/com/balun/sprintbootdemo/SprintBootDemoApplication.java
+++ src/main/java/com/balun/sprintbootdemo/SprintBootDemoApplication.java
@@ -2,13 +2,21 @@ package com.balun.sprintbootdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @SpringBootApplication
+@RestController
 public class SprintBootDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SprintBootDemoApplication.class, args);
 	}
-
+	@GetMapping("/")
+	public List<String> hello(){
+		return List.of("Hello", "World");
+	}
 
 }
