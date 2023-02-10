7fcfbfcde78ab8b0ede2649202af74a54bbdd3a9
Scott Balun <sbalun@gmail.com>
2023-02-09 20:29:11 -0500

    add the spring annotations to the service and controller classes for the dependency injection functionality to work

--- src/main/java/com/balun/sprintbootdemo/student/StudentController.java
+++ src/main/java/com/balun/sprintbootdemo/student/StudentController.java
@@ -1,6 +1,7 @@
 // This class handles the API layer in our n-tier architecture
 package com.balun.sprintbootdemo.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,7 +13,7 @@ import java.util.List;
 public class StudentController {
 
     private final StudentService studentService;
-
+    @Autowired // This annotation magically instantiates StudentService into this constructor
     public StudentController(StudentService studentService) {
         this.studentService = studentService;
     }
